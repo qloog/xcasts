1f03e53c51bf2b6d7aeefdb0c68d08a686be8669
qloog <quanlongwang@gmail.com>
2016-11-16 17:59:21 +0800

    change course schema

--- database/migrations/2016_11_15_171205_create_courses_table.php
+++ database/migrations/2016_11_15_171205_create_courses_table.php
@@ -13,11 +13,16 @@ class CreateCoursesTable extends Migration
 	 */
 	public function up()
 	{
-		Schema::create('courses', function(Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->increments('id');
-
+            $table->string('name')->comment('课程名称');
+            $table->text('description')->comment('课程描述');
+            $table->string('slug')->unique()->comment('slug');
+            $table->string('cover_image')->default('')->comment('课程封面图');
+            $table->integer('user_id')->unsigned()->default(0)->index()->comment('创建者id');
             $table->timestamps();
-		});
+            $table->softDeletes();
+        });
 	}
 
 	/**

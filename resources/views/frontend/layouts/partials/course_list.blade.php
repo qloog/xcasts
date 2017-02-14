9f7dd9ad5d0d285806b71d2bf417f97e5c0714f7
qloog <quanlongwang@gmail.com>
2017-02-14 18:50:09 +0800

    fix course list cover image

--- resources/views/frontend/layouts/partials/course_list.blade.php
+++ resources/views/frontend/layouts/partials/course_list.blade.php
@@ -4,7 +4,7 @@
     @foreach($courses as $course)
         <div class="ui raised link card">
             <a class="image" href="{{ route('course.show', $course->id) }}">
-                <img src="http://semantic-ui.com/images/avatar/large/steve.jpg" style="width: 357px; height: 210px;"/>
+                <img src="{{ $course->cover_image }}" style="width: 357px; height: 210px;"/>
             </a>
             <div class="content">
                 <a class="header" href="{{ route('course.show', $course->id) }}">{{ $course->name }}</a>

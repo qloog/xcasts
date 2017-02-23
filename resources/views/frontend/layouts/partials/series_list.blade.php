df0216819f9a439829e5f2adc0c4fbf78e011117
qloog <quanlongwang@gmail.com>
2017-02-23 19:01:36 +0800

    fix series url

--- resources/views/frontend/layouts/partials/series_list.blade.php
+++ resources/views/frontend/layouts/partials/series_list.blade.php
@@ -3,11 +3,11 @@
     @if((isset($series) && $series))
     @foreach($series as $item)
         <div class="ui raised link card">
-            <a class="image" href="{{ route('series.show', $item->id) }}">
+            <a class="image" href="{{ route('series.show', $item->slug) }}">
                 <img src="{{ $item->cover_image }}" style="width: 357px; height: 210px;"/>
             </a>
             <div class="content">
-                <a class="header" href="{{ route('series.show', $item->id) }}">{{ $item->name }}</a>
+                <a class="header" href="{{ route('series.show', $item->slug) }}">{{ $item->name }}</a>
                 <div class="meta">
                     <a class="time">{{ $item->created_at->diffForHumans() }}</a>
                     <span class="right floated">{{ $item->lessons->count() }} 视频</span>

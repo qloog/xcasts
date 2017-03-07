cc293de11b3e5b7d868747140827aadbcd9fe362
qloog <quanlongwang@gmail.com>
2017-03-07 22:43:23 +0800

    modify footer link

--- resources/views/frontend/layouts/partials/footer.blade.php
+++ resources/views/frontend/layouts/partials/footer.blade.php
@@ -8,42 +8,42 @@
             <div class="three wide column">
                 <h4 class="ui inverted small header">产品与服务</h4>
                 <div class="ui inverted list">
-                    <a href="#" class="item">VIP会员</a>
-                    <a href="#" class="item">学习路线</a>
-                    <a href="#" class="item">一对一</a>
+                    <a href="{{ url('vip') }}" class="item">VIP会员</a>
+                    <a href="{{ url('path') }}" class="item">学习路线</a>
+                    <a href="{{ url('one-to-one') }}" class="item">一对一</a>
                 </div>
             </div>
             <div class="three wide column">
                 <h4 class="ui inverted small header">支持</h4>
                 <div class="ui inverted list">
-                    <a href="#" class="item">问答社区</a>
-                    <a href="#" class="item">技术博客</a>
-                    <a href="#" class="item">Laravel官网</a>
+                    <a href="{{ route('topic.index') }}" class="item">问答社区</a>
+                    <a href="{{ url('blog') }}" class="item">技术博客</a>
+                    <a href="http://laravel.com" target="_blank" class="item">Laravel官网</a>
                 </div>
             </div>
             <div class="three wide column">
                 <h4 class="ui inverted small header">关于本站</h4>
                 <div class="ui inverted list">
-                    <a href="#" class="item">友情链接</a>
-                    <a href="#" class="item">社区规则</a>
-                    <a href="#" class="item">版权声明</a>
-                    <a href="#" class="item">合作</a>
+                    <a href="{{ url('link') }}" class="item">友情链接</a>
+                    <a href="{{ url('copyright') }}" class="item">社区规则</a>
+                    <a href="{{ url('copyright') }}" class="item">版权声明</a>
+                    {{--<a href="#" class="item">合作</a>--}}
                 </div>
             </div>
             <div class="two wide column">
                 <h4 class="ui inverted small header">服务提供商</h4>
-                <img class="ui image" src="/images/wireframe/image.png">
+                <img class="ui image" src="https://oj1ylm48b.qnssl.com/qiniu-white-195x105.png?e=1488901273&token=qvuNOgNkZMAJXYEPSHBT-6JEigk2AechM_fm57zS:YpYLdIknyqaL75clcpzgD5R9LWU">
             </div>
         </div>
 
         <div class="ui divider inverted"></div>
 
         <div class="ui inverted right floated horizontal list">
-            <div class="disabled item" href="#">运行于 UCloud 的 UHost 云主机上.</div>
+            <div class="disabled item" href="#">运行于 Aliyun 的 ECS 云主机上.</div>
         </div>
         <div class="ui inverted horizontal list">
-            <a class="disabled item">Developed By</a><a class="item" href="http://lnmp100.com">Qloog</a>,<a class="disabled item">Powered By</a><a class="item" href="https://laravel.com">Laravel</a>
-            <div class="item"><a href="{{ url('/service') }}">服务条款</a></div>
+            <a class="disabled item">Powered By</a><a class="item" href="https://laravel.com">Laravel</a>
+            <div class="item"><a href="{{ url('/terms') }}">服务条款</a></div>
             <div class="disabled item">© PHPCasts 2017. All rights reserved.</div>
         </div>
 

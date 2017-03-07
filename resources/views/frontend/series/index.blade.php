5f546767ccb36ca606fdc46731a72c061b7e917b
qloog <quanlongwang@gmail.com>
2017-03-07 22:01:58 +0800

    change type tab

--- resources/views/frontend/series/index.blade.php
+++ resources/views/frontend/series/index.blade.php
@@ -4,10 +4,12 @@
 
     <div class="ui center aligned header basic raised segment">
         <div class="teal ui buttons">
+            <a class="ui @if($type=='') active @endif large button" href="{{ route('series.index') }}">全部</a>
             <a class="ui @if($type=='backend') active @endif large button" href="{{ route('series.index', ['type' => 'backend']) }}">后端</a>
             <a class="ui @if($type=='service') active @endif large button" href="{{ route('series.index', ['type' => 'service']) }}">服务</a>
             <a class="ui @if($type=='frontend') active @endif large button" href="{{ route('series.index', ['type' => 'frontend']) }}">前端</a>
             <a class="ui @if($type=='tool') active @endif large button" href="{{ route('series.index', ['type' => 'tool']) }}">工具</a>
+            <a class="ui @if($type=='test') active @endif large button" href="{{ route('series.index', ['type' => 'test']) }}">测试</a>
         </div>
     </div>
 

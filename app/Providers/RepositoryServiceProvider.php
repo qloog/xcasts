58f33fcacdf459c55045f32627a1652f880fc44c
qloog <quanlongwang@gmail.com>
2017-03-17 22:53:53 +0800

    清理无用文件

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -26,10 +26,6 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(\App\Contracts\Repositories\UserRepository::class, \App\Repositories\Eloquent\UserRepositoryEloquent::class);
         $this->app->bind(\App\Contracts\Repositories\RoleRepository::class, \App\Repositories\Eloquent\RoleRepositoryEloquent::class);
         $this->app->bind(\App\Contracts\Repositories\PermissionRepository::class, \App\Repositories\Eloquent\PermissionRepositoryEloquent::class);
-        $this->app->bind(\App\Contracts\Repositories\NewsRepository::class, \App\Repositories\Eloquent\NewsRepositoryEloquent::class);
-        $this->app->bind(\App\Contracts\Repositories\NewsCategoryRepository::class, \App\Repositories\Eloquent\NewsCategoryRepositoryEloquent::class);
-        $this->app->bind(\App\Contracts\Repositories\AlbumRepository::class, \App\Repositories\Eloquent\AlbumRepositoryEloquent::class);
-        $this->app->bind(\App\Contracts\Repositories\EventRepository::class, \App\Repositories\Eloquent\EventRepositoryEloquent::class);
         $this->app->bind(\App\Contracts\Repositories\TopicRepository::class, \App\Repositories\Eloquent\TopicRepositoryEloquent::class);
         $this->app->bind(\App\Contracts\Repositories\SeriesRepository::class, \App\Repositories\Eloquent\SeriesRepositoryEloquent::class);
         $this->app->bind(\App\Contracts\Repositories\LessonRepository::class, \App\Repositories\Eloquent\LessonRepositoryEloquent::class);

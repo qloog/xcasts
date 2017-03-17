2cef53fe77948fca36a9939471b70c0490c4cf84
qloog <quanlongwang@gmail.com>
2017-03-17 22:54:40 +0800

    修改表名

--- database/migrations/2015_04_05_021502_create_posts_table.php
+++ database/migrations/2015_04_05_021502_create_posts_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePagesTable extends Migration {
+class CreatePostsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreatePagesTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('pages', function(Blueprint $table)
+		Schema::create('posts', function(Blueprint $table)
 		{
 			$table->increments('id');
             $table->string('title');
@@ -30,7 +30,7 @@ class CreatePagesTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('pages');
+		Schema::drop('posts');
 	}
 
 }

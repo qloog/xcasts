1d1c1764dacb1dcec4d8191583ff11586f62dc09
qloog <quanlongwang@gmail.com>
2017-05-15 23:45:42 +0800

    修改订阅项的高度

--- resources/views/frontend/plan/index.blade.php
+++ resources/views/frontend/plan/index.blade.php
@@ -16,6 +16,7 @@
             <div class="ui card">
                 <div class="content">
                     <div class="header center aligned">按月付</div>
+                    <div class="ui hidden divider"></div>
                     <div class="meta center aligned"><i class="massive teal child icon"></i> </div>
                     <div class="ui divider horizontal"><h1 class="ui teal header">69元</h1></div>
                     <div class="meta center aligned">试看一个月</div>
@@ -38,6 +39,7 @@
                         {{--<i class="icon plus"></i>--}}
                     {{--</a>--}}
                     <div class="header center aligned">按季付</div>
+                    <div class="ui hidden divider"></div>
                     <div class="meta center aligned"><i class="massive teal user icon"></i></div>
                     <div class="ui divider horizontal"><h1 class="ui teal header">199元</h1></div>
                     <div class="meta center aligned">先来一个季度</div>
@@ -57,6 +59,7 @@
             <div class="ui card">
                 <div class="content">
                     <div class="header center aligned">半年付</div>
+                    <div class="ui hidden divider"></div>
                     <div class="meta center aligned"><i class="massive centered teal student icon"></i></div>
                     <div class="ui divider horizontal"><h1 class="ui teal header">399元</h1></div>
                     <div class="meta center aligned">来它半年</div>
@@ -76,6 +79,7 @@
             <div class="ui card">
                 <div class="content">
                     <div class="header center aligned">按年付</div>
+                    <div class="ui hidden divider"></div>
                     <div class="meta center aligned"><i class="massive teal spy icon"></i></div>
                     <div class="ui divider horizontal"><h1 class="ui teal header">599元</h1></div>
                     <div class="meta center aligned">必须一年啊</div>

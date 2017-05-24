4912bd7ccd434fbb25fc1cc8f19b8f5c23ac3e5b
qloog <quanlongwang@gmail.com>
2017-05-24 19:01:15 +0800

    完善创建话题页相关的信息提示

--- resources/views/frontend/topic/create.blade.php
+++ resources/views/frontend/topic/create.blade.php
@@ -4,9 +4,22 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('simplemde/simplemde.min.css') }}">
 @endsection
 
+@section('title')
+    发布新话题 - 社区
+@endsection
+
 @section('content')
     <div class="ui grid">
         <div class="row"></div>
+        <div class="row">
+            <div class="thirteen wide column centered">
+                <div class="ui yellow message">
+                    我们希望 PHPCasts 能够成为拥有浓厚技术氛围的开发者社区，
+                    而实现这个目标，需要我们所有人的共同努力：友善，公平，尊重知识和事实。
+                    请严格遵守 - <a class="ui teal">社区发帖和管理规范</a>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="thirteen wide column centered">
                 <div class="ui grid">
@@ -18,11 +31,7 @@
                             @endforeach
                             </div>
                         @endif
-                        <div class="ui yellow message">
-                            我们希望 PHPCasts 能够成为拥有浓厚技术氛围的开发者社区，
-                            而实现这个目标，需要我们所有人的共同努力：友善，公平，尊重知识和事实。
-                            请严格遵守 - <a class="ui teal">社区发帖和管理规范</a>
-                        </div>
+
                         <div class="ui large middle aligned divided relaxed list padded segment">
                             <form class="ui form success" METHOD="post" action="{{ route('topic.store') }}">
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -43,10 +52,9 @@
                                     <div class="header">Form Completed</div>
                                     <div class="ui bulleted list">
 
-                                        <div class="item">请注意单词拼写，以及中英文排版，<a class="ui teal text colored">参考此页</a></div>
-                                        <div class="item">支持 Markdown 格式, <strong>**粗体**</strong>、~~删除线~~、<code>`单行代码`</code>, 更多语法请见这里 <a href="https://github.com/riku/Markdown-Syntax-CN/blob/master/syntax.md">Markdown 语法</a></div>
-                                        <div class="item">支持表情，使用方法请见 Emoji 自动补全来咯，可用的 Emoji 请见 :metal: :point_right: Emoji 列表  :star: :sparkles:</div>
-                                        <div class="item">上传图片, 支持拖拽和剪切板黏贴上传, 格式限制 - jpg, png, gif</div>
+                                        <div class="item">请注意单词拼写，以及中英文排版，<a class="ui teal text colored" href="https://github.com/sparanoid/chinese-copywriting-guidelines" target="_blank">参考此页</a></div>
+                                        <div class="item">支持 Markdown 格式, <strong>**粗体**</strong>、~~删除线~~、<code>`单行代码`</code>, 更多语法请见这里 <a href="https://github.com/riku/Markdown-Syntax-CN/blob/master/syntax.md" target="_blank">Markdown 语法</a></div>
+                                        {{--<div class="item">上传图片, 支持拖拽和剪切板黏贴上传, 格式限制 - jpg, png, gif</div>--}}
                                         <div class="item">发布框支持本地存储功能，会在内容变更时保存，「提交」按钮点击时清空</div>
                                     </div>
                                 </div>
@@ -60,23 +68,34 @@
                     </div>
                     <div class="four wide column">
                         <div class="ui card">
-                            <div class="image">
-                                <img src="/images/avatar2/large/kristy.png">
-                            </div>
                             <div class="content">
-                                <a class="header">Kristy</a>
+                                <a class="header">严禁发布以下类型信息</a>
                                 <div class="meta">
-                                    <span class="date">Joined in 2013</span>
+                                    <span class="date"></span>
                                 </div>
                                 <div class="description">
-                                    Kristy is an art director living in New York.
+                                    <div class="ui list">
+                                        <div class="item"><i class="right triangle icon"></i>请传播美好的事物，这里拒绝低俗、诋毁、谩骂等相关信息</div>
+                                        <div class="item"><i class="right triangle icon"></i>请尽量分享技术相关的话题，谢绝发布社会, 政治等相关新闻</div>
+                                        <div class="item"><i class="right triangle icon"></i>这里绝对不讨论任何有关盗版软件、音乐、电影如何获得的</div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="extra content">
-                                <a>
-                                    <i class="user icon"></i>
-                                    22 Friends
-                                </a>
+                        </div>
+                        <div class="ui card">
+                            <div class="content">
+                                <a class="header">在高质量优秀社区的我们</a>
+                                <div class="meta">
+                                    <span class="date"></span>
+                                </div>
+                                <div class="description">
+                                    <div class="ui list">
+                                        <div class="item"><i class="right triangle icon"></i>分享生活见闻, 分享知识</div>
+                                        <div class="item"><i class="right triangle icon"></i>接触新技术, 讨论技术解决方案</div>
+                                        <div class="item"><i class="right triangle icon"></i>发现更好工作机会</div>
+                                        <div class="item"><i class="right triangle icon"></i>甚至是开始另一个神奇的开源项目</div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>

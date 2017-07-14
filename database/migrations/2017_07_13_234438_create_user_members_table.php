fdcea8fe5b4d7a05bf4ba582db850020f0b49b08
qloog <quanlongwang@gmail.com>
2017-07-14 16:26:55 +0800

    add field for user members table

--- database/migrations/2017_07_13_234438_create_user_members_table.php
+++ database/migrations/2017_07_13_234438_create_user_members_table.php
@@ -13,7 +13,15 @@ class CreateUserMembersTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('user_members', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('level')->unsigned()->default(0)->comment('会员级别 1:月度 2:季度 3:半年 4:年卡 5:2年 6:3年');
+            $table->dateTime('start_time')->default('')->comment('开始时间');
+            $table->dateTime('end_time')->default('')->comment('结束时间');
+            $table->tinyInteger('status')->default(0)->comment('状态 0:无效 1:有效');
+            $table->tinyInteger('user_id')->unsigned()->default(0)->comment('uid');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +31,6 @@ class CreateUserMembersTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('user_members');
     }
 }

176ed3389e4d0479e1bede3d96fd3c672a48aa45
qloog <quanlongwang@gmail.com>
2017-07-27 22:14:08 +0800

    optimize success page

--- resources/views/frontend/feedback/success.blade.php
+++ resources/views/frontend/feedback/success.blade.php
@@ -3,11 +3,12 @@
 @section('content')
     <div class="ui container">
         <div class="ui hidden divider"></div>
-        <div class="ui positive message">
+        <div class="ui positive message"  style="height: 500px;">
             <div class="header">
                 感谢您的反馈^_^
             </div>
             <p><b><a href="{{ route('welcome') }}">回到首页</a></b></p>
-            <div class="ui hidden divider"></div>
         </div>
+        <div class="ui hidden divider"></div>
+    </div>
 @endsection

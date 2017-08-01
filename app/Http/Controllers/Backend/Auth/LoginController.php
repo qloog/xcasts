cf22508b0bc884751d8a494566524c5586ebb257
qloog <quanlongwang@gmail.com>
2017-08-01 21:47:50 +0800

    open admin login check

--- app/Http/Controllers/Backend/Auth/LoginController.php
+++ app/Http/Controllers/Backend/Auth/LoginController.php
@@ -60,4 +60,19 @@ class LoginController extends Controller
     {
         return Auth::guard('admin');
     }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->username() => 'required',
+            'password' => 'required',
+            'captcha' => 'required|captcha'
+        ]);
+    }
 }

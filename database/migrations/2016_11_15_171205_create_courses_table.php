51831acc0cb827210e48b9766ed949d02dca370b
qloog <quanlongwang@gmail.com>
2017-08-12 18:30:49 +0800

    fix class name

--- database/migrations/2016_11_15_171205_create_courses_table.php
+++ database/migrations/2016_11_15_171205_create_courses_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSeriesTable extends Migration
+class CreateCoursesTable extends Migration
 {
 
 	/**

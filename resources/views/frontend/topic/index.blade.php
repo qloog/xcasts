c3e62a34967d751f0a66fc7d8fe8d51cb3947ca8
qloog <quanlongwang@gmail.com>
2017-12-23 23:47:21 +0800

    fix topic index err

--- resources/views/frontend/topic/index.blade.php
+++ resources/views/frontend/topic/index.blade.php
@@ -27,8 +27,14 @@
                                         <span style="font-size: 12px; color: #ccc">
                                             <a class="item"><div class="ui horizontal label">{{ $topic->category->name }}</div></a>
                                             ⋅  <a href="{{ route('user.show', $topic->user_id) }}">{{ $topic->user->name }}</a>
+                                            @if($topic->last_reply_user_id > 0)
                                             ⋅ 于 {{ $topic->created_at->diffForHumans() }}
-                                            ⋅ 最后回复由 <a href="{{ route('user.show', $topic->last_reply_user_id) }}">{{ $topic->lastReplyUser->name }}</a> 于 {{ $topic->updated_at->diffForHumans() }}
+                                            ⋅ 最后回复由
+                                                <a href="{{ route('user.show', $topic->last_reply_user_id) }}">
+                                                    {{ $topic->lastReplyUser->name }}
+                                                </a>
+                                                于 {{ $topic->last_reply_time_at->diffForHumans() }}
+                                            @endif
                                         </span>
                                     </div>
                                 </div>

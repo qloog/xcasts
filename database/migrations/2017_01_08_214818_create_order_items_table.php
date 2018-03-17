0aa2d707d78c1651fd383b44ac14479626dde69a
qloog <quanlongwang@gmail.com>
2018-03-17 22:16:05 +0800

    修改订单设计之表字段

--- database/migrations/2017_01_08_214818_create_order_items_table.php
+++ database/migrations/2017_01_08_214818_create_order_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrderDetailsTable extends Migration
+class CreateOrderItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateOrderDetailsTable extends Migration
      */
     public function up()
     {
-        Schema::create('order_details', function(Blueprint $table) {
+        Schema::create('order_items', function(Blueprint $table) {
             $table->increments('id');
             $table->integer('order_id')->unsigned()->default(0)->comment('订单id');
-            $table->string('goods_id')->default(0)->comment('商品id');
-            $table->string('goods_name')->default('')->comment('商品名称');
-            $table->decimal('goods_price')->unsigned()->default(0.00)->comment('商品价格');
+            $table->integer('item_id')->default(0)->comment('商品id');
+            $table->string('name')->default('')->comment('商品名称');
+            $table->decimal('price')->unsigned()->default(0.00)->comment('商品价格');
             $table->integer('quantity')->default(0)->comment('购买总数');
-            $table->tinyInteger('user_id')->unsigned()->default(0)->comment('买家uid');
+            $table->integer('user_id')->unsigned()->default(0)->comment('买家uid');
             $table->softDeletes();
             $table->timestamps();
         });

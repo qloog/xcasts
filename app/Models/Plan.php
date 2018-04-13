2b7f4ca230f69aaaf9583ba59cfe63a7b9c0dc71
qloog <quanlongwang@gmail.com>
2018-04-13 11:23:55 +0800

    plan支持状态处理

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -12,7 +12,7 @@ class Plan extends Model implements Transformable
 
     protected $fillable = [
         'id', 'name', 'alias', 'introduction', 'price', 'promotion_price', 'promotion_start', 'promotion_end',
-        'user_id', 'created_at', 'updated_at'
+        'user_id', 'status', 'created_at', 'updated_at'
     ];
 
     public function user()

7f6095ffda492f735eac7be4aea11389a102a47f
qloog <quanlongwang@gmail.com>
2018-05-10 18:23:01 +0800

    修复一列显示缩小问题

--- resources/views/frontend/layouts/partials/course_list.blade.php
+++ resources/views/frontend/layouts/partials/course_list.blade.php
@@ -1,20 +1,22 @@
     <div class="row">
-        <div class="ui three stackable cards">
-            @if((isset($courses) && $courses))
-            @foreach($courses as $item)
-                <div class="ui card">
-                    <a class="image" href="{{ route('courses.show', $item->slug) }}">
-                        <img src="{{ thumb($item->cover_image, 357, 210) }}" style="height: 210px;"/>
-                    </a>
-                    <div class="content">
-                        <a class="center aligned header" href="{{ route('courses.show', $item->slug) }}">{{ $item->name }}</a>
-                    </div>
-                    <div class="extra content">
-                        <a class="time">{{ date('Y-m-d', strtotime($item->created_at)) }}</a>
-                        <span class="right floated">{{ $item->videos->count() }} 视频</span>
-                    </div>
-                </div>
-            @endforeach
-            @endif
+        <div class="column">
+            <div class="ui three stackable cards">
+                @if((isset($courses) && $courses))
+                    @foreach($courses as $item)
+                        <div class="ui centered card">
+                            <a class="image" href="{{ route('courses.show', $item->slug) }}">
+                                <img src="{{ thumb($item->cover_image, 357, 210) }}" style="height: 210px;"/>
+                            </a>
+                            <div class="content">
+                                <a class="center aligned header" href="{{ route('courses.show', $item->slug) }}">{{ $item->name }}</a>
+                            </div>
+                            <div class="extra content">
+                                <a class="time">{{ date('Y-m-d', strtotime($item->created_at)) }}</a>
+                                <span class="right floated">{{ $item->videos->count() }} 视频</span>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+            </div>
         </div>
     </div>

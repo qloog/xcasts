6828343efc4952ae27e0f12d44cfb107f62ffa0d
qloog <quanlongwang@gmail.com>
2018-05-19 18:16:31 +0800

    修改课程样式

--- resources/views/frontend/layouts/partials/course_list.blade.php
+++ resources/views/frontend/layouts/partials/course_list.blade.php
@@ -1,9 +1,9 @@
     <div class="row">
         <div class="column">
-            <div class="ui three stackable cards">
+            <div class="ui three stackable special cards">
                 @if((isset($courses) && $courses))
                     @foreach($courses as $item)
-                        <div class="ui card column grid">
+                        <div class="ui raised link card">
                             <a class="ui image" href="{{ route('courses.show', $item->slug) }}">
                                 @if ($item->is_publish == 2)
                                 <div class="ui red ribbon label">预告</div>

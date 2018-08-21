e7f2bef0e76f42aa0e345609a0b4bdaf1c26b603
qloog <quanlongwang@gmail.com>
2018-08-21 22:53:03 +0800

    修改banner

--- resources/views/frontend/course/detail.blade.php
+++ resources/views/frontend/course/detail.blade.php
@@ -6,25 +6,45 @@
 
 @section('description', $course->description)
 
+@section('styles')
+    <style type="text/css">
+        #particles-js {
+            position: absolute;
+            background: #323840;
+            left: 0;
+            right: 0;
+            top: 50px;
+            bottom: 0;
+            height:525px;
+            width: 100%;
+            z-index: -1;
+        }
+    </style>
+@endsection
+
 @section('content')
 
+    <!-- particles.js container -->
+    <div id="particles-js"></div>
+
     <!-- banner -->
-    <div class="ui vertical stripe segment" style="background:url('{{ thumb($course->cover_image, 1480, 525) }}')no-repeat 50% 0;height:525px">
-        <div class="ui middle aligned stackable grid container">
-            <div class="row">
-                {{--<div class="one wide column"></div>--}}
-                <div class="sixteen wide column" style="color: #ffffff;margin-top: 40px; background: rgba(0,0,0,0.3);padding: 30px 15px;">
-                    <h3>{{ $course->name }}</h3>
-                    <div class="label">{{ $course->description }}</div>
-                </div>
-                {{--<div class="one wide column"></div>--}}
-            </div>
+    {{--<div class="ui vertical stripe segment" style="background:url('{{ thumb($course->cover_image, 1480, 525) }}')no-repeat 50% 0;height:525px">--}}
+    <div class="ui vertical stripe segment" style="margin-top: 40px;">
+        {{--<div class="ui middle aligned stackable grid container">--}}
+            <div class="ui container">
+                    {{--<div class="one wide column"></div>--}}
+                    <div class="sixteen wide column" style="color: #ffffff; margin-top:-30px; background: rgba(0,0,0,0.3);padding: 30px 15px;">
+                        <h1>{{ $course->name }}</h1>
+                        <p class="label">{{ $course->description }}</p>
+                    </div>
+                    {{--<div class="one wide column"></div>--}}
+            {{--</div>--}}
         </div>
     </div>
 
     <!-- episode list-->
     <div class="ui vertical stripe segment" style="background-color: #E9EAED;">
-        <div class="ui middle aligned stackable grid container">
+        <div class="ui top aligned stackable grid container">
             <div class="row">
                 <div class="twelve wide column">
                     <table class="ui selectable  striped structured teal table">
@@ -98,9 +118,40 @@
                         </tbody>
                     </table>
                 </div>
+                <div class="four wide column">
+                @if(count($dependCourses))
+                    <!-- depend on courses-->
+                    <div class="ui segment">
+                        <h4 class="header">依赖的课程</h4>
+                        <div class="ui relaxed link list">
+                            @foreach($dependCourses as $item)
+                                <a class="@if(route('courses.show', ['slug'=> $item->slug]) == Request::url()) active @endif item" href="{{ route('courses.show', ['slug'=> $item->slug]) }}">
+                                    <i class="right triangle icon"></i>{{ $item->name }}
+                                </a>
+                            @endforeach
+                        </div>
+                    </div>
+                @endif
+
+                    <!-- weixin qrcode-->
+                    <div class="ui segment">
+                        <h4 class="header">关注公众号</h4>
+                        <img class="ui image" src="{{ cdn('/wechat_qrcode.jpg') }}" width="240px;">
+                    </div>
+                </div>
             </div>
             <div class="row"></div>
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    <script src="//cdn.bootcss.com/particles.js/2.0.0/particles.min.js"></script>
+    <script type="text/javascript">
+        /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+        particlesJS.load('particles-js', '/js/particles.json', function() {
+            console.log('callback - particles.js config loaded');
+        });
+    </script>
 @endsection
